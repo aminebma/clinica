710e35480d00fe05b8053f01e37bd4e8c9091cbd
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-05 18:53:34 +0100

    Adding accounts route, updating app.js with the new route and some express middlewares

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./config/index')
+const accounts = require('./routes/accounts')
+const patients = require('./routes/patients')
+const doctors = require('./routes/doctors')
 const app = express()
 
-const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(helmet())
+app.use(morgan('tiny'))
+app.use('/api/accounts', accounts)
+app.use('/api/patients', patients)
+app.use('/api/doctors', doctors)
 
 mongoose.connect(config.getDbConnectionString(), {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.connection.once('open',function(){
     console.log('Successfully connected to the database')
 }).on('error', function(error){
-    console.log('Failed to login to the database.\n',error)
+    console.log('Failed connecting to the database.\n',error)
 })
 
-app.listen(port, '127.0.0.1')
+//Port
+const port = process.env.PORT || 3000
+app.listen(port, '127.0.0.1', () => console.log(`Listening on port ${port}`))
