c08845c9499fe4a1d1a26fd3c2402b144089539f
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-05 17:03:45 +0100

    Adding config files, and test and models folders. Updating app.js to turn on the server listening on port 3000 on localhost and connecting to the MongoDB database

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express')
 const mongoose = require('mongoose')
+const config = require('./config/index')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+mongoose.connect(config.getDbConnectionString(), {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.once('open',function(){
+    console.log('Successfully connected to the database')
+}).on('error', function(error){
+    console.log('Failed to login to the database.\n',error)
+})
+
+app.listen(port, '127.0.0.1')
