95ab18424f8bbac1a067091dbf0cdcea24f6412e
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-08-30 19:01:15 +0100

    Adding animation when tapping the doctor's list elements and expanding the card to prepare the request dialog

--- lib/pages/request.dart
+++ lib/pages/request.dart
@@ -1,1 +1,55 @@
 import 'package:flutter/material.dart';
+
+import 'package:clinica/models/doctor.dart';
+
+class Request extends StatelessWidget {
+  final Doctor _doctor;
+
+  Request(this._doctor);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        extendBodyBehindAppBar: true,
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Hero(
+                tag: 'doctor-${_doctor.id}',
+                child: Image.network(
+                  "${_doctor.picture}",
+                  scale: 1.0,
+                  repeat: ImageRepeat.noRepeat,
+                  fit: BoxFit.fitWidth,
+                  height: MediaQuery.of(context).size.height / 3,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ListTile(
+                title: Text(
+                  '${_doctor.firstName} ${_doctor.lastName}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                subtitle: Text('${_doctor.speciality}\n${_doctor.phoneNumber}'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/doctorsList.dart
+++ lib/widgets/doctorsList.dart
@@ -4,6 +4,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:clinica/models/doctor.dart';
 import 'package:clinica/services/doctors.dart';
 import 'package:clinica/widgets/listFilter.dart';
+import 'package:clinica/pages/request.dart';
 
 class DoctorsList extends StatefulWidget {
   @override
@@ -73,71 +74,67 @@ class _DoctorsListState extends State<DoctorsList> {
                     return TweenAnimationBuilder(
                       duration: Duration(milliseconds: 300),
                       tween: Tween<double>(begin: 15, end: 0),
-                      child: Card(
-                        elevation: 7.0,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(color: Colors.grey[400], width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                        child: Column(
-                          children: [
-                            Image.network(
-                              "${snapshot.data[index].picture}",
-                              scale: 1.0,
-                              repeat: ImageRepeat.noRepeat,
-                              fit: BoxFit.fill,
-                              height: 250,
-                            ),
-                            Divider(
-                              height: 10,
-                            ),
-                            Text(
-                              "${snapshot.data[index].firstName} ${snapshot.data[index].lastName}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Divider(
-                              height: 10,
-                            ),
-                            Text(
-                              "${snapshot.data[index].speciality}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 19,
-                              ),
-                            ),
-                            Divider(
-                              height: 10,
-                            ),
-                            Text(
-                              "${snapshot.data[index].phoneNumber}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 19,
-                              ),
-                            ),
-                            RaisedButton.icon(
-                              icon: Icon(
-                                Icons.question_answer,
-                                color: Colors.blue,
-                              ),
-                              label: Text(
-                                "Demander un diagnostique",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.blue,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      Request(snapshot.data[index])));
+                        },
+                        child: Card(
+                          elevation: 7.0,
+                          shape: RoundedRectangleBorder(
+                            side:
+                                BorderSide(color: Colors.grey[400], width: 1.0),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                          child: Column(
+                            children: [
+                              Hero(
+                                tag: 'doctor-${snapshot.data[index].id}',
+                                child: Image.network(
+                                  "${snapshot.data[index].picture}",
+                                  scale: 1.0,
+                                  repeat: ImageRepeat.noRepeat,
+                                  fit: BoxFit.fill,
+                                  height: 250,
                                 ),
                               ),
-                              onPressed: () {
-                                print('Demande de diagnostique envoyée !');
-                              },
-                              color: Colors.white,
-                            ),
-                          ],
+                              Divider(
+                                height: 10,
+                              ),
+                              Text(
+                                "${snapshot.data[index].firstName} ${snapshot.data[index].lastName}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Divider(
+                                height: 10,
+                              ),
+                              Text(
+                                "${snapshot.data[index].speciality}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 19,
+                                ),
+                              ),
+                              Divider(
+                                height: 10,
+                              ),
+                              Text(
+                                "${snapshot.data[index].phoneNumber}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 19,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       builder:
